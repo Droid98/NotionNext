c45edb1b78b911d8a8f46fd4fdc511570e03c62b
Droid98 <hulkflame98@gmail.com>
2025-09-03 16:32:14 +0800

    Add files via upload

--- themes/plog/components/Footer.js
+++ themes/plog/components/Footer.js
@@ -1,20 +1,44 @@
-import Vercel from '@/components/Vercel'
 import { siteConfig } from '@/lib/config'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { isMobile } from '@/lib/utils'
 
-export const Footer = (props) => {
-  const d = new Date()
-  const currentYear = d.getFullYear()
+/**
+ * 网站底部
+ * @param {*} props
+ * @returns
+ */
+const Footer = props => {
+  const router = useRouter()
+  return (
+    <footer className='relative w-full bg-gray-900 text-gray-400 border-t border-gray-700 mt-20 p-8'>
+      <div className='max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center text-sm'>
+        <div className='flex items-center space-x-2 text-center md:text-left mb-4 md:mb-0'>
+          <span>
+            © {siteConfig('SINCE')}{' '}
+            <a href={siteConfig('LINK')} className='text-white hover:text-gray-300 transition-colors'>
+              {siteConfig('AUTHOR')}
+            </a>
+          </span>
+          <span className='hidden md:inline-block'>•</span>
+          <a
+            href='https://beian.miit.gov.cn/'
+            target='_blank'
+            rel='noreferrer'
+            className='hover:text-white transition-colors'>
+            {siteConfig('BEI_AN')}
+          </a>
+        </div>
 
-  const since = siteConfig('SINCE')
-  const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
-
-  return <footer className={'z-10 relative mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all' } >
-     <div className="my-4 text-sm leading-6">
-       <div className="flex align-baseline justify-start flex-wrap space-x-6">
-         <div> © {siteConfig('AUTHOR')} {copyrightDate}  </div>
-         <div>Powered By <a href="https://github.com/tangly1024/NotionNext" className='underline'>NotionNext {siteConfig('VERSION')}</a></div>
-         <Vercel />
-       </div>
-     </div>
-   </footer>
+        <div className='text-center md:text-right'>
+          <a href={siteConfig('LINK')}>{siteConfig('TITLE')}</a>
+          <p className='text-xs mt-1'>
+            Powered by Notion + Next.js
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
 }
+
+export default Footer
