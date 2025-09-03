ce00f2f3a87e43674c913aeeacad4df4d5b1cf98
Droid98 <hulkflame98@gmail.com>
2025-09-03 16:56:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -7,30 +7,53 @@ const BLOG = {
     '02ab3b8678004aa69e9e415905ef32a5,en:7c1d570661754c8fbc568e00a01fd70e',
   THEME: process.env.NEXT_PUBLIC_THEME || 'plog', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
-  SINCE: process.env.NEXT_PUBLIC_SINCE || 2021, // e.g if leave this empty, current year will be used.
+  SINCE: process.env.NEXT_PUBLIC_SINCE || 2017, // e.g if leave this empty, current year will be used.
 
-  PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
-  NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 60, // 更新缓存间隔 单位(秒)；即每个页面有60秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
-  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
-  APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
+  PSEUDO_TITLE: process.env.NEXT_PUBLIC_PSEUDO_TITLE || 'MK写真馆', // 显示在网页标签的标题
+  META_KEYWORDS: process.env.NEXT_PUBLIC_META_KEYWORDS || 'MK写真馆, 旅拍, 摄影, 个人写真', // 网站关键词
+  // 页脚展示的版权
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'MK写真馆', // e.g 'Justin Mason'
+  EMAIL: process.env.NEXT_PUBLIC_EMAIL || 'tangly1024@gmail.com', // 邮箱
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.notion.so/mk-photo', // 链接
+  BIO: process.env.NEXT_PUBLIC_BIO || 'A developer that loves reading, traveling and cooking', // 个人简介
+  DESCRIPTION: process.env.NEXT_PUBLIC_DESCRIPTION || 'A website built with Next.js and Notion API', // 网站描述
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'MK写真馆', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '收录最美瞬间', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://xn--mk-u34c463lf79a.dpdns.org', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '仓木麻衣, Mai Kuraki, MK写真馆', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
-  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
-  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
+  // 社交媒体链接，留空将不显示；所有图标皆可在这里找到 https://fontawesome.com/icons?d=gallery&s=brands&m=free
+  SOCIAL_LINK: process.env.NEXT_PUBLIC_SOCIAL_LINK || 'https://github.com/tangly1024/NotionNext',
+  SOCIAL_WEIBO: process.env.NEXT_PUBLIC_SOCIAL_WEIBO || '',
+  SOCIAL_X: process.env.NEXT_PUBLIC_SOCIAL_X || '',
+  SOCIAL_MASTODON: process.env.NEXT_PUBLIC_SOCIAL_MASTODON || '',
+  SOCIAL_DOUBAN: process.env.NEXT_PUBLIC_SOCIAL_DOUBAN || '',
+  SOCIAL_LINKEDIN: process.env.NEXT_PUBLIC_SOCIAL_LINKEDIN || '',
+  SOCIAL_TWITCH: process.env.NEXT_PUBLIC_SOCIAL_TWITCH || '',
+  SOCIAL_INSTAGRAM: process.env.NEXT_PUBLIC_SOCIAL_INSTAGRAM || 'https://www.instagram.com/notion.next.js',
+  SOCIAL_QUORA: process.env.NEXT_PUBLIC_SOCIAL_QUORA || '',
+  SOCIAL_YOUTUBE: process.env.NEXT_PUBLIC_SOCIAL_YOUTUBE || '',
+  SOCIAL_BILIBILI: process.env.NEXT_PUBLIC_SOCIAL_BILIBILI || 'https://space.bilibili.com/12971253',
+  SOCIAL_REDDIT: process.env.NEXT_PUBLIC_SOCIAL_REDDIT || '',
+  SOCIAL_RSS: process.env.NEXT_PUBLIC_SOCIAL_RSS || '/feed/rss.xml',
+  SOCIAL_ZHIHU: process.env.NEXT_PUBLIC_SOCIAL_ZHIHU || '',
+  SOCIAL_TELEGRAM: process.env.NEXT_PUBLIC_SOCIAL_TELEGRAM || '',
+  SOCIAL_EMAIL: process.env.NEXT_PUBLIC_SOCIAL_EMAIL || 'droid1998a@gmail.com',
+  SOCIAL_TIKTOK: process.env.NEXT_PUBLIC_SOCIAL_TIKTOK || '',
+  SOCIAL_PINTEREST: process.env.NEXT_PUBLIC_SOCIAL_PINTEREST || '',
+  SOCIAL_STACK_OVERFLOW: process.env.NEXT_PUBLIC_SOCIAL_STACK_OVERFLOW || '',
+  SOCIAL_WECHAT: process.env.NEXT_PUBLIC_SOCIAL_WECHAT || '',
+  SOCIAL_TWITTER: process.env.NEXT_PUBLIC_SOCIAL_TWITTER || '',
+  SOCIAL_FACEBOOK: process.env.NEXT_PUBLIC_SOCIAL_FACEBOOK || '',
+  SOCIAL_JUEJIN: process.env.NEXT_PUBLIC_SOCIAL_JUEJIN || '',
+  SOCIAL_XIAOHONGSHU: process.env.NEXT_PUBLIC_SOCIAL_XIAOHONGSHU || '',
+  SOCIAL_QQ: process.env.NEXT_PUBLIC_SOCIAL_QQ || '',
 
-  // RSS订阅
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能
+  // 站点配置
+  ...require('./conf/site.config'), // 站点
+  ...require('./conf/menu.config'), // 菜单
+  ...require('./conf/map.config'), // 地图
+  ...require('./conf/lang.config'), // 语言
+  ...require('./conf/notion.config'), // Notion 相关
 
-  // 其它复杂配置
-  // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
-  ...require('./conf/comment.config'), // 评论插件
-  ...require('./conf/contact.config'), // 作者联系方式配置
-  ...require('./conf/post.config'), // 文章与列表配置
-  ...require('./conf/analytics.config'), // 站点访问统计
+  // 模板配置
+  ...require('./conf/theme.config'), // 主题
   ...require('./conf/image.config'), // 网站图片相关配置
   ...require('./conf/font.config'), // 网站字体
   ...require('./conf/right-click-menu'), // 自定义右键菜单相关配置
@@ -54,19 +77,8 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
 
   // 文章列表相关设置
-  CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
-
-  // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
-  LAYOUT_SIDEBAR_REVERSE:
-    process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || false,
-
-  // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
-  GREETING_WORDS:
-    process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
-
-  // uuid重定向至 slug
-  UUID_REDIRECT: process.env.UUID_REDIRECT || false
+  POST_LIST_STYLE: process.env.NEXT_PUBLIC_POST_LIST_STYLE || 'page', // ['page', 'scroll']
+  POSTS_PER_PAGE: process.env.NEXT_PUBLIC_POSTS_PER_PAGE || 12, // 每一页展示的博客文章数
 }
 
 module.exports = BLOG
