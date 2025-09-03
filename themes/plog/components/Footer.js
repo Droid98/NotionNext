ae36093ec4a15dc9fc89c3f77f4d5d24f6cd4f8b
Droid98 <hulkflame98@gmail.com>
2025-09-03 16:47:06 +0800

    Update Footer.js

--- themes/plog/components/Footer.js
+++ themes/plog/components/Footer.js
@@ -33,12 +33,13 @@ const Footer = props => {
         <div className='text-center md:text-right'>
           <a href={siteConfig('LINK')}>{siteConfig('TITLE')}</a>
           <p className='text-xs mt-1'>
-            Powered by Notion + Next.js
+            Powered by NotionNext 4.9.0
           </p>
         </div>
       </div>
     </footer>
   )
 }
+
 
 export default Footer
