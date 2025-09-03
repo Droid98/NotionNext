81f3e402b617bc3595cd5b87eafe3a13e72f066b
Droid98 <hulkflame98@gmail.com>
2025-09-03 14:56:49 +0800

    Update BlogPost.js

--- themes/plog/components/BlogPost.js
+++ themes/plog/components/BlogPost.js
@@ -34,14 +34,15 @@ const BlogPost = (props) => {
             data-aos-duration="500"
             data-aos-once="true"
             data-aos-anchor-placement="top-bottom"
-            key={post?.id} className='plog-card-item cursor-pointer relative'>
-            <LazyImage src={pageThumbnail} className='aspect-[16/9] w-full h-full object-cover filter contrast-120' />
+            key={post?.id} className='cursor-pointer relative'>
+
+            <LazyImage src={pageThumbnail} className='aspect-[16/9] w-full h-full object-cover filter contrast-120' title={''} />
 
             <h2 className="text-md absolute left-0 bottom-0 m-4 text-gray-100 shadow-text">
                 {siteConfig('POST_TITLE_ICON') && <NotionIcon icon={post.pageIcon} />} {post?.title}
             </h2>
-            {post?.category && <div className='text-xs rounded-lg absolute left-0 top-0 m-4 px-2 py-1 bg-gray-200 dark:bg-black dark:text-gray-400 opacity-60'>
-                {post?.category}
+            {post?.category && <div className='text-xs rounded-lg absolute left-0 top-0 m-4 px-2 py-1 bg-gray-200 dark:bg-black dark:text-gray-400'>
+                {post.category}
             </div>}
         </article>
   )
